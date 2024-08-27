903cde7e107b5a87a2b72f1d1bb1a78d3fad13df
Pratishtha Gaur <gaurpratishtha1603@gmail.com>
2024-08-27 17:53:56 +0530

    changes in home

--- easy-celebrate/src/pages/home/Home.jsx
+++ easy-celebrate/src/pages/home/Home.jsx
@@ -4,9 +4,19 @@ import "./home.css";
 
 function Home() {
     return (
-        <div>
-          Home
-        </div>
+      <div>
+        <section class="ftco-section ftco-intro" style="background-image: url('https://cdn.pixabay.com/photo/2020/05/31/19/26/cherries-5243873_640.jpg');" data-stellar-background-ratio="0.5">
+    	<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row justify-content-center">
+    			<div class="col-md-8 text-center">
+    				<h2>Together we will explore new things</h2>
+    				<a href="" class="icon-video d-flex align-items-center justify-content-center"><span class="fa fa-play"></span></a>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+      </div>
     );
 }
 
